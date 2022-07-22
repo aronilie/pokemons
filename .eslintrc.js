517964cae1880ac30ef40fd7e6148f53d16e0420
Aron Ilie <aronilie.code@gmail.com>
2022-07-22 19:03:55 +0200

    Configured eslint and typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: ["airbnb-base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "tsconfig.json",
     ecmaVersion: "latest",
     sourceType: "module",
   },
