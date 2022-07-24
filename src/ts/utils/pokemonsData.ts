ce83b0dedce6e88e12924c3c427d1d21c2188401
Aron Ilie <aronilie.code@gmail.com>
2022-07-24 15:09:22 +0200

    Added getAllPokemonsUrls function to pokemonsData file

--- src/ts/utils/pokemonsData.ts
+++ src/ts/utils/pokemonsData.ts
@@ -1,8 +1,10 @@
-import { Pokemon } from "../types/interfaces.js";
+import { Pokemon, PokemonData as IPokemonData } from "../types/interfaces.js";
 
 export const url = "https://pokeapi.co/api/v2/pokemon/?offset=00&limit=50";
 
 export const pokemons: Pokemon[] = [];
+
+export const allPokemonsUrls: string[] = [];
 
 export const addPokemon = async (pokemonUrl: string) => {
   const response = await fetch(pokemonUrl);
@@ -30,4 +32,13 @@ export const addPokemon = async (pokemonUrl: string) => {
   };
 
   pokemons.push(pokemon);
+};
+
+export const getAllPokemonsUrls = async (pokemonsListUrl: string) => {
+  const response = await fetch(pokemonsListUrl);
+  const data: IPokemonData = await response.json();
+
+  data.results.forEach((PokemonData) => {
+    allPokemonsUrls.push(PokemonData.url);
+  });
 };
