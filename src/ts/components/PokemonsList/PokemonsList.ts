ab60b81dd56b5c9c401e03dcfdada17dd2f2d917
Aron Ilie <aronilie.code@gmail.com>
2022-07-24 11:14:59 +0200

    Setted private parameter pokemon on constructor of PokemonsList

--- src/ts/components/PokemonsList/PokemonsList.ts
+++ src/ts/components/PokemonsList/PokemonsList.ts
@@ -2,7 +2,7 @@ import Component from "../Component/Component.js";
 import { Component as IComponent, Pokemon } from "../../types/interfaces.js";
 
 class PokemonsList extends Component implements IComponent {
-  constructor(parent: HTMLElement, pokemon: Pokemon) {
+  constructor(parent: HTMLElement, private pokemon: Pokemon) {
     super(parent, "pokemons-list", "ul");
   }
 }
