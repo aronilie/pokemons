02058cc5b52ca4071669857b84e41fed5a71f145
Aron Ilie <aronilie.code@gmail.com>
2022-07-24 18:51:44 +0200

    Created PokemonList class

--- src/ts/components/PokemonsList/PokemonsList.ts
+++ src/ts/components/PokemonsList/PokemonsList.ts
@@ -1,10 +1,29 @@
 import Component from "../Component/Component.js";
-import { Component as IComponent, Pokemon } from "../../types/interfaces.js";
+import {
+  Component as IComponent,
+  PokemonData as IPokemonData,
+} from "../../types/interfaces.js";
+import PokemonCard from "../PokemonCard/PokemonCard.js";
 
 class PokemonsList extends Component implements IComponent {
-  constructor(parent: HTMLElement, private pokemon: Pokemon) {
+  parent;
+
+  constructor(parent: HTMLElement, private urlOfAllPokemons: string) {
     super(parent, "pokemons-list", "ul");
+
+    this.parent = parent;
+
+    this.render();
   }
+
+  render = async () => {
+    const response = await fetch(this.urlOfAllPokemons);
+    const data: IPokemonData = await response.json();
+
+    data.results.forEach((PokemonData) => {
+      new PokemonCard(this.parent, PokemonData.url);
+    });
+  };
 }
 
 export default PokemonsList;
